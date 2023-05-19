7754e71dc96659c8b04d93d7cea6594e31416953
Ashma Upreti <ashma.upreti99@gmail.com>
2023-05-18 23:19:27 -0400

    Used HTML semantics to manage content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1>hello</h1>
-     <p>jkdsnfkdsjnkdckdjnc</p>
+      <header>this is header</header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Projects</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+      <aside>
+        <h3>fun stuffs</h3>
+        <p>These are the fun stuffs you can do with sementics</p>
+      </aside>
+      <main>
+        <h2>Our Articles</h2>
+        <article>
+          <h2>First Article</h2>
+          <p>This the the first article</p>
+        </article>
+        <article>
+          <h2>Second Article</h2>
+          <p>This the the second article</p>
+        </article>
+      </main>
+      <footer>this is footerfooter</footer>
     </>
-  )
+  );
 }
-
-export default App
+export default App;
