dd01ccb5c847ee3872f945ac9cac89d1f37a7177
Ashma Upreti <ashma.upreti99@gmail.com>
2023-05-18 23:54:21 -0400

    styled the content

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,49 @@ import "./App.css";
 function App() {
   return (
     <>
-      <header>this is header</header>
+      <header>Front End Development</header>
       <nav>
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Projects</li>
-          <li>Contact Us</li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Our Projects</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact Us</a>
+          </li>
         </ul>
       </nav>
       <aside>
         <h3>fun stuffs</h3>
         <p>These are the fun stuffs you can do with sementics</p>
+        <h3>Ads</h3>
+        <p>These could be ads</p>
       </aside>
       <main>
-        <h2>Our Articles</h2>
-        <article>
-          <h2>First Article</h2>
-          <p>This the the first article</p>
-        </article>
-        <article>
-          <h2>Second Article</h2>
-          <p>This the the second article</p>
-        </article>
+        <h2></h2>
+        <section>
+          <article>
+            <h2>First Article</h2>
+            <p>This the first article</p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h2>Second Article</h2>
+            <p>This the second article</p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h2>Third Article</h2>
+            <p>This the the third article</p>
+          </article>
+        </section>
       </main>
       <footer>this is footerfooter</footer>
     </>
